23d28ca87caef44caa881e5f81cdc7fd56f856cd
oht <duduio2050@gmail.com>
2024-08-27 00:16:51 +0900

    과제 6단계 지연 로딩 테스트 완료

---
+++ src/test/java/org/sparta/jpaschedule/ScheduleTest.java
@@ -0,0 +1,24 @@
+package org.sparta.jpaschedule;
+
+
+import org.junit.jupiter.api.Test;
+import org.sparta.jpaschedule.schedule.dto.response.ScheduleResponseDto;
+import org.sparta.jpaschedule.schedule.entity.Schedule;
+import org.sparta.jpaschedule.schedule.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ScheduleTest {
+
+    @Autowired
+    ScheduleRepository scheduleRepository;
+
+    @Test
+    void testSchedule() {
+
+        // 지연 로딩 테스트
+        Schedule schedule = scheduleRepository.findById(4L).orElse(null);
+
+    }
+}
